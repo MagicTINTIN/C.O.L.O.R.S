71b6f09776651592512715d2d6239c630c9ca0b7
MagicTINTIN <valentinservieres@gmail.com>
2023-11-19 10:45:16 +0200

    listening to space keyup

---
+++ scripts/colors.js
@@ -0,0 +1,9 @@
+document.getElementById("body").addEventListener("keyup", (e) => {
+    if (e.key === " ")
+        generateNewColors();
+    //console.log(`Key "${e.key}" released [event: keyup]`, e);
+});
+
+function generateNewColors() {
+    console.log("NOT IMPLEMENTED YET");
+}
