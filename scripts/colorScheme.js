99237f12bc97eefe6b26560932045dfb910350cf
MagicTINTIN <valentinservieres@gmail.com>
2023-12-01 23:56:46 +0200

    grouping function in one main (still wip)

--- scripts/colorScheme.js
+++ scripts/colorScheme.js
@@ -1,71 +1,60 @@
 window.schemeUsed = 0
 schemesIDs = {
-    randomValue:0,
-    monochromaticValue:1,
-    analogousValue:2,
-    complementaryValue:3,
-    splitcomplementaryValue:4,
-    triadicValue:5,
-    squareValue:6,
-    rectangleValue:7,
+    randomValue: 0,
+    monochromaticValue: 1,
+    analogousValue: 2,
+    complementaryValue: 3,
+    splitcomplementaryValue: 4,
+    triadicValue: 5,
+    squareValue: 6,
+    rectangleValue: 7,
 }
 
-function generateMonochromatic(color, n) {
-    const [r, g, b] = hexToRgb(color);
-    const step = 20;
-    const monochromaticColors = [];
+const NORMAL_DISTRIBUTION = 0;
+const MONOCHROMATIC_DISTRIBUTION = 1;
+const ANALOGOUS_DISTRIBUTION = 2;
+const SPLIT_COMP_DISTRIBUTION = 3;
+const RECTANGLE_DISTRIBUTION = 4;
 
-    for (let i = 0; i < n; i++) {
-        const newColor = rgbToHex(r, g, b + i * step);
-        monochromaticColors.push(newColor);
-    }
+function generateGeometricalColors(base, n, angle, type = NORMAL_DISTRIBUTION) {
+    const baseColor = hexToRgb(base);
+    const hsvBase = RGBtoHSV(baseColor)
 
-    return monochromaticColors;
-}
-
-function generateAnalogous(color, n) {
-    const [r, g, b] = hexToRgb(color);
-    const angleStep = 30;
-    const analogousColors = [];
-
-    for (let i = 0; i < n; i++) {
-        const hslColor = `hsl(${(i * angleStep) % 360}, 100%, 50%)`;
-        analogousColors.push(hslColor);
-    }
-
-    return analogousColors;
-}
-
-// Function to generate split complementary colors
-function generateSplitComplementary(color) {
-    const [r, g, b] = hexToRgb(color);
-    const angle = 30; // Adjust this value to control the color difference
-    const splitComplementaryColors = [
-        color,
-        `hsl(${(180 + angle) % 360}, 100%, 50%)`,
-        `hsl(${(180 - angle) % 360}, 100%, 50%)`
-    ];
-
-    return splitComplementaryColors;
-}
-
-function generateGeometricalColors(base, n, angle, rectangle = false) {
-    const [r, g, b] = hexToRgb(color);
-    const angle = 60; // Adjust this value to control the color difference
     const geometryColors = [color];
 
-
-    for (let i = 1; i < n / 2; i++) {
-        const hslColor1 = `hsl(${(angle * i) % 360}, 100%, 50%)`;
-        const hslColor2 = `hsl(${(angle * (i + 3)) % 360}, 100%, 50%)`;
-        geometryColors.push(hslColor1, hslColor2);
+    if (type == NORMAL_DISTRIBUTION) {
+        for (let i = 1; i < n; i++) {
+            let hsvColor = { h: (angle * i) % 360, }
+            const dhslColor = rgbToHex(HSVtoRGB());
+            geometryColors.push(hslColor);
+        }
     }
+    else if (type == MONOCHROMATIC_DISTRIBUTION) {
+        for (let i = 0; i < n; i++) {
+            const newColor = rgbToHex(r, g, b + i * step);
+            monochromaticColors.push(newColor);
+        }
+    }
+    else if (type == RECTANGLE_DISTRIBUTION) {
+        for (let i = 1; i < n / 2; i++) {
+            const hslColor1 = `hsl(${(angle * i) % 360}, 100%, 50%)`;
+            const hslColor2 = `hsl(${(angle * (i + 3)) % 360}, 100%, 50%)`;
+            geometryColors.push(hslColor1, hslColor2);
+        }
+    }
+    else if (type == SPLIT_COMP_DISTRIBUTION) {
+        const splitComplementaryColors = [
+            color,
+            `hsl(${(180 + angle) % 360}, 100%, 50%)`,
+            `hsl(${(180 - angle) % 360}, 100%, 50%)`
+        ];
 
-
-    for (let i = 1; i < n; i++) {
-        let hsvColor = {h:(angle * i) % 360, }
-        const dhslColor = rgbToHex(HSVtoRGB());
-        geometryColors.push(hslColor);
+    }
+    else if (type == ANALOGOUS_DISTRIBUTION) {
+        for (let i = 0; i < n; i++) {
+            const hslColor = `hsl(${(i * angleStep) % 360}, 100%, 50%)`;
+            analogousColors.push(hslColor);
+        }
     }
 
     return geometryColors;
@@ -84,8 +73,21 @@ function generateSquare(color, n) {
 }
 
 function generateRectangle(color, n) {
-    return generateGeometricalColors(color, n, 60, true);
+    return generateGeometricalColors(color, n, 60, RECTANGLE_DISTRIBUTION);
 }
+
+function generateMonochromatic(color, n) {
+    return generateGeometricalColors(color, n, 0, MONOCHROMATIC_DISTRIBUTION);
+}
+
+function generateSplitComplementary(color, angle = 30) {
+    return generateGeometricalColors(color, n, angle, SPLIT_COMP_DISTRIBUTION);
+}
+
+function generateAnalogous(color, n, angleStep = 30) {
+    return generateGeometricalColors(color, n, angleStep, ANALOGOUS_DISTRIBUTION);
+}
+
 
 const startingColor = "#FF0000";
 const numberOfColors = 5;
